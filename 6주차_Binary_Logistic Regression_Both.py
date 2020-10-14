1ce594eee5d5d06da0feeefed28d42d1d1ac3284
heonsooo <68042068+heonsooo@users.noreply.github.com>
2020-10-15 08:48:49 +0900

    Add files via upload

---
+++ 6주차_Binary_Logistic Regression_Both.py
@@ -0,0 +1,99 @@
+# from sklearn.metrics import precision_score, recall_score, f1_score
+# from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import pymysql
+import numpy as np
+
+
+
+def classification_performance_eval(y, y_predict):
+    tp, tn, fp, fn = 0,0,0,0
+    
+
+    for y, yp in zip(y,y_predict):
+        if y == 1 and yp ==1 :
+            tp += 1 
+        elif y == 1 and yp == -1 :
+            fn += 1 
+        elif y == -1 and yp == 1 :
+            fp += 1 
+        elif y == -1 and yp == -1 :
+            tn += 1 
+    
+    # print(tp, tn, fp, fn)
+
+    try :
+        accuracy = (tp+ tn)/(tp+tn+fp+fn)
+        precision = (tp)/(tp+fp)
+        recall = (tp)/(tp+fn)
+        f1_score = 2*precision*recall / (precision + recall)
+        
+    except:
+        if tp == 0: 
+            precision, recall, f1_score = 0 ,0 ,0 
+    
+    return accuracy, precision, recall, f1_score
+
+conn = pymysql.connect(host = 'localhost', user = 'Soo', password = '1234' , db = 'data_science')
+curs = conn.cursor(pymysql.cursors.DictCursor)
+
+sql = 'select*from db_score'
+curs.execute(sql)
+data = curs.fetchall()
+
+curs.close()
+conn.close()
+
+X = [ ( t['homework'], t['discussion'], t['midterm'] )  for t in data ]
+Y = [ 1 if (t['grade'] == 'B')  else -1 for t in data ]
+X_train , X_test , Y_train, Y_test = train_test_split(X, Y, test_size = 0.333 , random_state = 42) 
+
+Lr = LogisticRegression(C=1)
+Lr.fit(X_train, Y_train)
+predict = Lr.predict(X_test)
+
+acc, prec, rec, f1 = classification_performance_eval(Y_test, predict)
+print( ' < train_test_split by_Logistic Regression_Binary_tts > ')
+print('accuracy = %.4f' %acc)
+print('precision = %.4f' %prec)
+print('recall = %.4f ' %rec)
+print('F1_score = %.4f' %f1, '\n')
+
+
+from sklearn.model_selection import KFold
+
+accuracy = []
+precision = []
+recall = []
+f1_score = []
+
+kf = KFold(n_splits = 3, random_state =42, shuffle = True)
+
+for train_index, test_index in kf.split(X):
+
+    X = np.array(X)
+    Y = np.array(Y)
+    x_train , x_test = X[train_index], X[test_index]
+    y_train , y_test = Y[train_index], Y[test_index]
+    
+
+    Lr_k_fold = LogisticRegression(C=1, random_state=0)
+    Lr_k_fold.fit(x_train, y_train)
+    y_predict = Lr_k_fold.predict(x_test)
+    
+
+    acc, prec, rec, f1 = classification_performance_eval(y_test, y_predict)
+    accuracy.append(acc)
+    precision.append(prec)
+    recall.append(rec)
+    f1_score.append(f1)
+
+    import statistics 
+
+
+print( ' < K-fold cross validation by_Logistic Regression-Binary > ')
+print('accuracy_평균 = ', round(statistics.mean(accuracy),4))
+print('precision_평균 = ', round(statistics.mean(precision),4))
+print('recall_평균 = ', round(statistics.mean(recall),4))
+print('f1_score_평균 = ', round(statistics.mean(f1_score),4),'\n')
---
+++ Multi_all.py
@@ -0,0 +1,148 @@
+import pymysql
+import numpy as np
+from sklearn.model_selection import KFold   
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+from sklearn import svm                                 # SVM 
+
+from sklearn.preprocessing import StandardScaler        # Random forest 사용
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.linear_model import LogisticRegression     # Logistical Regression 
+
+
+####################3
+def start():
+    conn = pymysql.connect(host = 'localhost', user = 'Soo', password = '1234' , db = 'data_science')
+    curs = conn.cursor(pymysql.cursors.DictCursor)
+    sql = 'select*from db_score'
+    curs.execute(sql)
+
+    data = curs.fetchall()
+    curs.close()
+    conn.close()
+
+    X = [ ( t['homework'], t['discussion'], t['midterm'] )  for t in data ]
+    y = [ 0 if (t['grade'] == 'A')  else 1 if (t['grade'] == 'B') else 2 for t in data ]            # muliti class 
+
+    # y = [ 1 if (t['grade'] == 'B') else -1 for t in data ]                                        # binary 
+    X = np.array(X)
+    y = np.array(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33 , random_state=42)
+    return X_train, X_test, y_train, y_test 
+    ################### 고정 #############
+
+def calcurator_tts(y_test, y_predict): 
+    cnf_matrix = confusion_matrix(y_test, y_predict)
+
+
+    FP = cnf_matrix.sum(axis=0) - np.diag(cnf_matrix) 
+    FN = cnf_matrix.sum(axis=1) - np.diag(cnf_matrix)
+    TP = np.diag(cnf_matrix)
+    TN = cnf_matrix.sum() - (FP + FN + TP)
+    FP, FN, TP, TN =  FP.astype(float), FN.astype(float) , TP.astype(float), TN.astype(float)
+   
+    ACC = (TP+TN)/(TP+FP+FN+TN)
+    prec =TP/(TP+FP)
+    recall = TP/(TP+FN)
+    f1_score = 2*prec*recall / (prec + recall)
+    print('accuracy =', ACC)
+    print('precision = ' ,prec)
+    print('recall =  ' ,recall)
+    print('F1_score = ', f1_score, '\n')
+
+def calcurator_kfold(X, y,a):
+    F1_score , Recall, Precision, Accuracy = [], [], [], []
+    mean_acc ,mean_prec , mean_rec, mean_f1 = [0,0,0],[0,0,0],[0,0,0],[0,0,0]
+    k= int(4)
+    kf = KFold(n_splits =k, random_state = 42, shuffle = True)
+
+
+    for train_index, test_index in kf.split(X):
+        X_train , X_test = X[train_index], X[test_index]
+        y_train , y_test = y[train_index], y[test_index]
+
+        select_model(a)
+        
+
+
+
+
+    for i in range(0, k):
+        for j in range(0,3):
+            mean_acc[j] += float(Accuracy[i][j])
+            mean_prec[j] += float(Precision[i][j])
+            mean_rec[j] += float(Recall[i][j])
+            mean_f1[j] += float(F1_score[i][j])
+
+    for j in range(0,3):
+        mean_acc[j] = round(mean_acc[j]/k,4)
+        mean_prec[j] = round(mean_prec[j]/k,4)
+        mean_rec[j] =round(mean_rec[j]/k,4)
+        mean_f1[j] = round(mean_f1[j]/k,4)
+    print('accuracy_평균 = ', mean_acc)
+    print('precision_평균 = ', mean_prec )
+    print('recall_평균 = ', mean_rec)
+    print('f1_score_평균 = ',mean_f1,'\n')
+    print( ' < K-fold cross validation by_',name)
+
+    
+
+
+
+ def select_model(a):
+    if a == 'svm' :
+        SVM_MUlti_Class(X_train, X_test,y_train, y_test )
+
+    elif a == 'rF' :
+        Random_Forest_Multi_Class(X_train, X_test,y_train, y_test)
+
+    elif a =='LR':
+        Logistic_Regression_Multi_Class(X_train, X_test,y_train, y_test)
+
+
+
+
+def SVM_MUlti_Class(X_train, X_test,y_train, y_test ):                      #이거만 완성
+    classifier = svm.SVC(kernel='linear').fit(X_train, y_train)
+    y_predict = classifier.predict(X_test)
+    cnf_matrix = confusion_matrix(y_test, y_predict)
+    name = 'SVM_Multi-Class'
+    return cnf_matrix, name
+
+
+def Random_Forest_Multi_Class(X_train, X_test,y_train, y_test ): 
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42).fit(X_train, y_train)  # n_estimatiors : tree 개수 
+    y_predict = classifier.predict(X_test)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42).fit(X_train, y_train)  # n_estimatiors : tree 개수 
+    y_predict = classifier.predict(X_test)
+    cnf_matrix = confusion_matrix(y_test, y_predict)
+
+
+    calcurator_tts(y_test, y_predict)
+    
+    
+    
+    name = 'Random_Forest_Multi-Class'
+    
+    return cnf_matrix, name
+
+
+def Logistic_Regression_Multi_Class(X_train, X_test,y_train, y_test):
+    Lr = LogisticRegression(C=1, random_state=0, solver= 'liblinear' ).fit(X_train, y_train)
+    y_predict = Lr.predict(X_test)
+    cnf_matrix = confusion_matrix(y_test, y_predict)
+    name = 'Logistic Regression_Multi-Class'
+    return cnf_matrix, name
+
+
